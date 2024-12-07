4ac468e51ed266245c6eef0a400563a71634d7a2
Ertugrul Eren Selcuk <83607933+ertugrulerenselcuk@users.noreply.github.com>
2024-12-07 21:47:23 +0300

    distinctSql.sql

---
+++ distinctSql.sql
@@ -0,0 +1,9 @@
+/* find how mant different ratings there are in the film table
+sort the returned list in reverse
+
+db : DVDRental
+table: film 
+columns: rating */
+
+select distinct rating
+from film;
